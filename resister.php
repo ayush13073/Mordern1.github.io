38c8dc998ef732780ad20687fea7ef8dd212262e
ayush13073 <104487018+ayush13073@users.noreply.github.com>
2022-05-17 17:23:17 +0530

    Update resister.php

--- resister.php
+++ resister.php
@@ -130,7 +130,7 @@ mysqli_stmt_close($stmt);
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-secondary ml-2" value="Reset">
             </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+            <p>Already have an account? <a href="index.php">Login here</a>.</p>
         </form>
     </div>    
 </body>
